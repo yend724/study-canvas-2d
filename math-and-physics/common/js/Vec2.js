99ee552449ed62e5f598bd4417397d484dd2276b
yend724 <yend9524@gmail.com>
2021-08-17 20:59:05 +0900

    Update Vec2 Class

--- math-and-physics/common/js/Vec2.js
+++ math-and-physics/common/js/Vec2.js
@@ -3,9 +3,22 @@ class Vec2 {
     this.x = x;
     this.y = y;
   }
+
   add(v) {
     this.x += v.x;
     this.y += v.y;
+    return this;
+  }
+
+  sub(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+    return this;
+  }
+
+  multi(s) {
+    this.x *= s;
+    this.y *= s;
     return this;
   }
 }
